13f448ca5c76804ef51f4dec7f409f4185804819
woorinlee <lenya97@naver.com>
2022-06-14 02:00:13 +0900

    update bottom navigation view function

--- Android Application/SmartHome05/app/src/main/java/com/lh089/smarthome05/MainActivity.java
+++ Android Application/SmartHome05/app/src/main/java/com/lh089/smarthome05/MainActivity.java
@@ -7,11 +7,16 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    BottomNavigationView bottomNavigationView;
+
+    View frame1, frame2, frame3, frame4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +25,50 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        frame1 = findViewById(R.id.frame1);
+        frame2 = findViewById(R.id.frame2);
+        frame3 = findViewById(R.id.frame3);
+        frame4 = findViewById(R.id.frame4);
+        frame1.setVisibility(View.VISIBLE);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.tab1:
+                        frame1.setVisibility(View.VISIBLE);
+                        frame2.setVisibility(View.GONE);
+                        frame3.setVisibility(View.GONE);
+                        frame4.setVisibility(View.GONE);
+                        return true;
+                    case R.id.tab2:
+                        frame1.setVisibility(View.GONE);
+                        frame2.setVisibility(View.VISIBLE);
+                        frame3.setVisibility(View.GONE);
+                        frame4.setVisibility(View.GONE);
+                        return true;
+                    case R.id.tab3:
+                        frame1.setVisibility(View.GONE);
+                        frame2.setVisibility(View.GONE);
+                        frame3.setVisibility(View.VISIBLE);
+                        frame4.setVisibility(View.GONE);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.bluetooth_active:
-                Toast.makeText(this, "Bluetooth", Toast.LENGTH_SHORT).show();
+                frame1.setVisibility(View.GONE);
+                frame2.setVisibility(View.GONE);
+                frame3.setVisibility(View.GONE);
+                frame4.setVisibility(View.VISIBLE);
             default: 
                 return super.onOptionsItemSelected(item);
         }
