a384be8f13d77a16236ad9439c59aa6b9a9b040f
woorinlee <lenya97@naver.com>
2022-06-14 00:26:17 +0900

    add smarthome application source file (ver 5)

---
+++ Android Application/SmartHome05/app/src/main/java/com/lh089/smarthome05/MainActivity.java
@@ -0,0 +1,40 @@
+package com.lh089.smarthome05;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.bluetooth_active:
+                Toast.makeText(this, "Bluetooth", Toast.LENGTH_SHORT).show();
+            default: 
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+}
